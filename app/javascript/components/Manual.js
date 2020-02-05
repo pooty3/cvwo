154474a49f1163b069fe5b4365266805ce39e239
pooty3 <aloysiuslim.dw@gmail.com>
2020-02-05 19:10:41 +0800

    manual edits

--- app/javascript/components/Manual.js
+++ app/javascript/components/Manual.js
@@ -8,6 +8,8 @@ return(
 <p>About- About the app</p>
 <p>Taskpage - Shows the taskpage</p>
 <p>To add a task, use the add task button.</p>
+<p>You can edit and delete task here</p>
+<p>You can view tasks with selected tag here</p>
 <p>Warning: Task with empty title will not be added</p>
 <p>Tagpage - Shows the tags you are currently using</p>
 <p>You can add a tag here (must not be empty and must be unique!)</p>
