97f7ed84781b27f6a458ab344af4389a0b151ccc
pooty3 <aloysiuslim.dw@gmail.com>
2020-02-05 19:14:29 +0800

    edited about

--- app/javascript/components/About.js
+++ app/javascript/components/About.js
@@ -5,7 +5,7 @@ const About=()=>{
 		<div>
 		<h1>About</h1>
 		<h3>This is the best To-do App you can find around in the internet!</h3>
-		<h3>Developed by Lord Pooty Sama the Great</h3>
+		<h3>Developed by Your Majesty!</h3>
 		<h5>All rights reserved!</h5></div>
 
 
